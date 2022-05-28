865e6deeb27f9b127f206b935026fa4e57e881c1
Andrea Tomarelli <andrea.tomarelli@gmail.com>
2022-05-28 13:22:23 +0200

    eslint setup

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,24 +1,27 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
-import { HomePage } from './home.page';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { IonicModule } from '@ionic/angular'
+import { HomePage } from './home.page'
 
 describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+  let component: HomePage
+  let fixture: ComponentFixture<HomePage>
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      declarations: [
+        HomePage,
+      ],
+      imports: [
+        IonicModule.forRoot(),
+      ],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    fixture = TestBed.createComponent(HomePage)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  }))
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
